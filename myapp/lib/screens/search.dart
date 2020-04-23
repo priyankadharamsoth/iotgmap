fb7d74f5796211b273ca27c1a6d2338da888cd28
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2020-04-23 17:10:54 +0530

    Removed bug in yaml file

--- myapp/lib/screens/search.dart
+++ myapp/lib/screens/search.dart
@@ -1,30 +1,21 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Search extends StatelessWidget{
+class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: <Widget>[
+        body: Column(
+      children: <Widget>[
         Container(
-          height: MediaQuery.of(context).size.height/3,
+          height: MediaQuery.of(context).size.height / 3,
           width: MediaQuery.of(context).size.width,
           child: GoogleMap(
             
-            
-          )
-            
-            
-        
+          ),
         )
-      ],)
-    );
-      
-
+      ],
+    ));
   }
-}
-
-class GoogleMap {
 }
