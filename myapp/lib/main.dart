b61155bc251a7d3711ac7c8d7ec9dc019c0b15ba
priyanka <priyanka.d17@iiits.in>
2020-04-24 13:03:25 +0530

    added currentposition

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/services/geolacator_service.dart';
 import 'package:myapp/screens/search.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final locatorService = GeolocatorService();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Parking app',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
+    return FutureProvider(
+          create :(context) => locatorService.getLocation(),
+          child: MaterialApp(
+        title: 'Parking app',
+        theme: ThemeData(
+          
+          primarySwatch: Colors.blue,
+        ),
+        home:Search(),
       ),
-      home:Search(),
     );
+    
   }
 }
 
