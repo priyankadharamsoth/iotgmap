5d516d7391534ab9807b3939c8867b50f4d68865
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2020-04-24 15:07:22 +0530

    renamed getlocation function and removed auto generated function code for geting geo location

--- myapp/lib/services/geolacator_service.dart
+++ myapp/lib/services/geolacator_service.dart
@@ -2,11 +2,8 @@ import 'package:geolocator/geolocator.dart';
 
 class GeolocatorService{
 
-  Future<Position>getloacation()async{
+  Future<Position>getLocation()async{
     var geolocator = Geolocator();
     return await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high,locationPermissionLevel: GeolocationPermission.location);
   }
-
-  getLocation() {}
-
 }
