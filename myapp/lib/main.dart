eb55a4b139aecc0f5e24d0c659929820c5a579f3
priyanka <priyanka.d17@iiits.in>
2020-04-25 15:30:13 +0530

    nearplaces

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:myapp/models/place.dart';
+import 'package:myapp/screens/places_service.dart';
 import 'package:myapp/services/geolacator_service.dart';
 import 'package:myapp/screens/search.dart';
 import 'package:provider/provider.dart';
@@ -7,11 +10,20 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   final locatorService = GeolocatorService();
+  final placesService = PlacesService();
 
   @override
   Widget build(BuildContext context) {
-    return FutureProvider(
-          create :(context) => locatorService.getLocation(),
+    return MultiProvider(
+      providers :[
+          FutureProvider(create :(context) => locatorService.getLocation()),
+          ProxyProvider<Position,Future<List<Place>>>(
+            update:(context,position,places){
+              return (position != null) ? placesService.getPlaces(position.latitude,position.longitude): null;
+            },
+          )
+      ],
+        
           child: MaterialApp(
         title: 'Parking app',
         theme: ThemeData(
